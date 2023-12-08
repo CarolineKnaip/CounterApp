bfc2945e4819276becf28cfaaafbc0f3f49ed0bf
CarolineKnaip <carol.knaip@yahoo.com.br>
2023-12-08 03:12:13 -0300

    Initial commit Counter App

---
+++ README.md
@@ -0,0 +1,30 @@
+# IceCream Counter App
+
+## Description
+The *IceCream Counter App* is a simple and efficient application designed to help ice cream shops monitor the number of customers entering the establishment. With a user-friendly interface and intuitive features, the app simplifies the process of counting people, providing more effective management of customer flow.
+
+## Key Features
+1. **Precise Counting:** Use the counting feature to accurately record the number of customers entering the ice cream shop.
+2. **Intuitive Interface:** A simple and intuitive user interface, making customer counting an easy and quick task.
+
+## App Download
+   - [Coming soon on the Google Play Store](#)
+
+## How to Use
+   - Use the increase and decrease buttons to adjust the count as customers enter or exit.
+
+
+# IceCream Counter App - PORTUGUÊS / BRASIL
+
+## Descrição
+O *IceCream Counter App* é uma aplicação simples e eficiente projetada para ajudar sorveterias a monitorarem o número de clientes que entram no estabelecimento. Com uma interface amigável e recursos intuitivos, o aplicativo facilita o processo de contagem de pessoas, proporcionando uma gestão mais eficaz do fluxo de clientes.
+
+## Recursos Principais
+1. **Contagem Precisa:** Utilize a funcionalidade de contagem para registrar com precisão o número de clientes que entram na sorveteria.
+2. **Interface Intuitiva:** Uma interface de usuário simples e intuitiva, tornando a contagem de clientes uma tarefa fácil e rápida.
+
+## Download do Aplicativo
+   - [Em breve na Google Play Storee](#)
+
+## Como Usar
+   - Utilize os botões de aumento e diminuição para ajustar a contagem conforme os clientes entram ou saem.
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void decrement() {
+    setState(() {
+      count--;
+    });
+    print(count);
+  }
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+    print(count);
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/sorveteria.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? 'Está lotado' : 'Pode entrar!',
+              style: TextStyle(
+                fontSize: 30,
+                color: isFull ? Colors.red : Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Text(
+                count.toString(),
+                style: TextStyle(
+                  fontSize: 100,
+                  color: isFull ? Colors.red : Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Sair',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                ),
+                const SizedBox(
+                  width: 64,
+                ),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.red : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
